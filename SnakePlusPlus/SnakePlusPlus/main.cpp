3f9690a89f3cd51b396d4dc8f8ea6f5bd021da89
Robert Altner <robert-altner@t-online.de>
2018-06-05 18:59:27 +0200

    Removed Console

--- SnakePlusPlus/SnakePlusPlus/main.cpp
+++ SnakePlusPlus/SnakePlusPlus/main.cpp
@@ -1,6 +1,7 @@
 #include "Framework.h"
+#include <Windows.h>
 
-int main()
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 
 	Framework::Initialize(WIDTH, HEIGHT, "SnakePlusPlus v1.0", sf::Style::Close);
